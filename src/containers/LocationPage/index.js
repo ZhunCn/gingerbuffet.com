977b5a2c8d8125227ac6da045891f5eb073dfaa8
Randy <randyzhunc@gmail.com>
2019-11-26 15:26:51 -0600

    Update Mobile Location Page

--- src/containers/LocationPage/index.js
+++ src/containers/LocationPage/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import NavigationBar from '../../components/NavigationBar'
 import HolidayMessage from '../../components/HolidayMessage'
 import Iframe from 'react-iframe';
-import { Grid, Segment, Header } from 'semantic-ui-react';
+import { Grid, Segment, Header, Responsive } from 'semantic-ui-react';
 import './index.css';
 import Footer from '../../components/Footer'
 
@@ -16,36 +16,56 @@ const locationQuery = '&q=Ginger+Buffet,Fairview+Heights+IL'
 
 export default class Location extends React.Component {
   render() {
+    const mapDesktopIFrame = <Iframe
+      url={mapAPIURL + API_KEY + locationQuery}
+      width='100%'
+      height='450vh'
+    />;
+    const mapMobileIFrame = <Iframe
+      url={mapAPIURL + API_KEY + locationQuery}
+      width='100%'
+      height='200vh'
+    />;
+
+    const businessAddress = <Segment>
+      <Header>
+        Ginger Buffet and Grill
+      </Header>
+      53 Ludwig Drive <br />
+      Suite #102 <br />
+      Fairview Heights, IL 62208<br />
+      Phone: (618)-398-3888
+      </Segment>;
     return (
       <div className='App-background'>
-          <NavigationBar currentPage='location' />
-          <div className='content'>
-          <HolidayMessage />
+        <NavigationBar currentPage='location' />
+        <div className='content'>
 
-          <Grid divided='vertically'>
-            <Grid.Row columns={2}>
-              <Grid.Column>
-          <Segment>
-            <Header>
-              Ginger Buffet and Grill
-            </Header>
-            53 Ludwig Drive <br/>
-            Suite #102 <br/>
-            ​Fairview Heights, IL 62208<br/>
-            Phone: (618)-398-3888
-          </Segment>
-          </Grid.Column>
-          <Grid.Column>
-          <Iframe 
-          url={mapAPIURL + API_KEY + locationQuery}
-          width='100%'
-          height='450vh'
-          />
-          </Grid.Column>
-          </Grid.Row>
-          </Grid>
-          </div>
-          <Footer currentPage='location' />
+          <HolidayMessage />
+          <Responsive minWidth={Responsive.onlyTablet.minWidth}>
+
+
+            <Grid divided='vertically'>
+              <Grid.Row columns={2}>
+                <Grid.Column>
+                  {businessAddress}
+                </Grid.Column>
+                <Grid.Column>
+                  {mapDesktopIFrame}
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Responsive>
+
+          <Responsive {...Responsive.onlyMobile} >
+            {businessAddress}
+
+            {mapMobileIFrame}
+
+          </Responsive>
+        </div>
+
+        <Footer currentPage='location' />
       </div>
     );
   }
