4e4b5b3f6d3fbb71ad45534dab2bec93ff61e302
Randy <randyzhunc@gmail.com>
2020-02-17 06:16:02 -0500

    Remove PropTypes

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types'
 import NavigationBar from '../../components/NavigationBar'
 import HolidayMessage from '../../components/HolidayMessage'
 import { Header } from 'semantic-ui-react'
